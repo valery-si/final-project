02d8f9715b66c75de0e21169dafa686878e2784b
Valery S <silsilgpt@gmail.com>
2026-05-01 20:32:57 +0300

    remove server and client docker file, they are not in use anymore

--- scripts/start-infra.ps1
+++ scripts/start-infra.ps1
@@ -3,4 +3,4 @@ $ErrorActionPreference = "Stop"
 $root = Split-Path -Parent $PSScriptRoot
 Set-Location $root
 
-docker compose -f docker-compose.yml -f docker-compose.gpu.yml up -d db pgadmin
+docker compose -f docker-compose.yml up -d db pgadmin
